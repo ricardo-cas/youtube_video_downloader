352876523d1d11b59edb56c969da6cb961a49455
Ricardo <ricardo_cas4@hotmail.com>
2020-10-06 22:20:59 -0300

    definido o tamanho maximo e minimo da janela

--- main.py
+++ main.py
@@ -9,11 +9,17 @@ master.title("Youtube Downloader") # define o título da janela
 
 master.iconbitmap('.\icone.ico') ## Definindo o ícone do aplicativo
 
+master.minsize(300,300) # definindo o tamanho mínimo da janela
+master.maxsize(500,500) # definindo o tamanho maximo da janela
+
+
+
+
 # Labels
 
-Label(master, text="Download Converter Youtube", fg="red", font=("Calibri",15)).grid(sticky=N,padx=100,row=0)
+Label(master, text="Baixar Vídeos do Youtube", fg="red", font=("Calibri",15)).grid(sticky=N,padx=100,row=0)
 
-Label(master, text="Cole o link do vídeo no campo abaixo para baixar:", fg="blue", font=("Calibri",12)).grid(sticky=N,pady=15,row=1)
+Label(master, text="Cole o link do vídeo no campo abaixo para baixar:", font=("Calibri",12)).grid(sticky=N,pady=15,row=1)
 
 
 
