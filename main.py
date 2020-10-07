5fc824ec946fc39402e264d95d422b42daa14e01
Ricardo <ricardo_cas4@hotmail.com>
2020-10-06 22:03:59 -0300

    Tela principal criada e adicionado alguns textos na tela

---
+++ main.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from pytube import YouTube
+from PIL import ImageTk, Image
+
+# Tela principal
+master = Tk() # inicia a instancia do TKinter
+
+master.title("Youtube Downloader") # define o título da janela
+
+master.iconbitmap('.\icone.ico') ## Definindo o ícone do aplicativo
+
+# Labels
+
+Label(master, text="Download Converter Youtube", fg="red", font=("Calibri",15)).grid(sticky=N,padx=100,row=0)
+
+Label(master, text="Cole o link do vídeo no campo abaixo para baixar:", fg="blue", font=("Calibri",15)).grid(sticky=N,pady=15,row=1)
+
+
+
+
+master.mainloop() # obrigatório para a execução do programa
+
+
+
+# print('Hello World')
