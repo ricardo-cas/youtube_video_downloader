6e00d8db9813f03bb39f2f45ff33e87da69d5d3c
Ricardo <ricardo_cas4@hotmail.com>
2020-10-06 23:48:36 -0300

    adicionado o .gitignore

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from tkinter import *
 from pytube import YouTube
+import os
+import pytube
+
+# Funções
+
+def download():
+    url_video       = url.get()
+    try:
+        youtube     = pytube.YouTube(url_video)
+        video       = youtube.streams.first()
+        caminho_video = "C:/Users/Pichau/Desktop/estudos_programacao/youtube_video_downloader/Arquivo/"
+        video.download(caminho_video.encode(encoding='UTF-8',errors='strict'))
+        notificacao.config(fg="green", text= "Download concluído")
+
+    except Exception as e:
+        print(e)
+        # notificacao.config(fg="red", text= "Vídeo não baixado")
+        
+
 
 
 # Tela principal
@@ -7,7 +26,7 @@ master = Tk() # inicia a instancia do TKinter
 
 master.title("Youtube Downloader") # define o título da janela
 
-master.iconbitmap('.\icone.ico') ## Definindo o ícone do aplicativo
+master.iconbitmap('./icone.ico') ## Definindo o ícone do aplicativo
 
 master.minsize(300,300) # definindo o tamanho mínimo da janela
 master.maxsize(500,500) # definindo o tamanho maximo da janela
@@ -32,7 +51,8 @@ url = StringVar()
 Entry(master, width=50,textvariable=url).grid(sticky=N, row=2)
 
 # Botões
-Button(master, text="Baixar Vídeo", font=("Calibri",12)).grid(sticky=N,pady=15,row=3)
+
+Button(master, text="Baixar Vídeo", font=("Calibri",12),command=download).grid(sticky=N,pady=15,row=3)
 
 
 
